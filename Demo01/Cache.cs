da9097a1768dff8950a8abd27e0718a657e06fb3
shenqiangbin <shenqiangbin@163.com>
2017-01-31 12:51:19 +0800

    最基本的缓存

---
+++ Demo01/Cache.cs
@@ -0,0 +1,31 @@
+﻿using Memcached.ClientLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo01
+{
+    public class Cache
+    {
+        public static MemcachedClient Client;
+
+        static Cache()
+        {
+            SockIOPool pool = SockIOPool.GetInstance();
+            InitPool(pool);
+
+            Client = new MemcachedClient();
+            Client.EnableCompression = false;
+        }
+
+        static void InitPool(SockIOPool pool)
+        {
+            string[] services = { "127.0.0.1:11211" };
+            pool.SetServers(services);
+
+            pool.Initialize();
+        }
+    }
+}
--- Demo01/Program.cs
+++ Demo01/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Memcached.ClientLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,11 @@ namespace Demo01
     {
         static void Main(string[] args)
         {
+            Cache.Client.Add("01", "Hello");
+            var s = Cache.Client.Get("01");
+            Console.WriteLine(s.ToString());
+
+            Console.ReadKey();
         }
     }
 }
