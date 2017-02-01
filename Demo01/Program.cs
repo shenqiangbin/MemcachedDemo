06a4196137e76c798aa94a2099e94ad7e0689dcb
shenqiangbin <shenqiangbin@163.com>
2017-02-01 20:16:31 +0800

    Cient Library 添加列表缓存示例

--- Demo01/Program.cs
+++ Demo01/Program.cs
@@ -13,9 +13,33 @@ namespace Demo01
         {
             Cache.Client.Add("01", "Hello");
             var s = Cache.Client.Get("01");
+
+            //Cache.Client.Delete("01");
+            //Cache.Client.Set("01", "hello");
+
             Console.WriteLine(s.ToString());
+
+            List<User> users = new List<User>
+            {
+                new User { Id = 1,Name = "admin1" },
+                new User { Id = 2,Name = "admin2" },
+            };
+
+            Cache.Client.Set("users", users);
+
+            //get the users
+
+            var usersObj = Cache.Client.Get("users");
+
 
             Console.ReadKey();
         }
+    }
+
+    [Serializable]
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
